32af53262b8dcef45498f940e656d71985b43380
m00n <mjm3204@naver.com>
2026-05-15 10:32:15 +0900

    Service: ProjcetMember

--- src/main/java/com/nhnacademy/taskAPI/exception/BusinessException.java
+++ src/main/java/com/nhnacademy/taskAPI/exception/BusinessException.java
@@ -5,7 +5,6 @@ public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
--- src/main/java/com/nhnacademy/taskAPI/task/TaskMilestoneRequest.java
+++ src/main/java/com/nhnacademy/taskAPI/task/TaskMilestoneRequest.java
@@ -3,6 +3,6 @@ package com.nhnacademy.taskAPI.task;
 import jakarta.validation.constraints.NotNull;
 
 public record TaskMilestoneRequest(
-        @NotNull Long milestoneId
+        Long milestoneId
 ) {
 }
