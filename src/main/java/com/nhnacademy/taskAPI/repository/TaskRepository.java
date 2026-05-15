d4a9801d61e170bdf4e86f0887c867a2b3e1fc5a
m00n <mjm3204@naver.com>
2026-05-15 15:15:17 +0900

    수정

--- src/main/java/com/nhnacademy/taskAPI/repository/TaskRepository.java
+++ src/main/java/com/nhnacademy/taskAPI/repository/TaskRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByProject_ProjectId(Long projectId);
+    List<Task> findByMilestone_MilestoneIdAndProject_ProjectId(Long milestoneId, Long projectId);
+    Optional<Task> findByTaskIdAndProject_ProjectId(Long taskId, Long projectId);
 }
