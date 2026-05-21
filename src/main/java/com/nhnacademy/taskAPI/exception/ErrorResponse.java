dfe4eb57519dfe8b135d26a5fbe3f578245e6db3
OiJs <oijs9663@gmail.com>
2026-05-21 09:43:06 +0900

    feat: ErrorResponse 수정

--- src/main/java/com/nhnacademy/taskAPI/exception/ErrorResponse.java
+++ src/main/java/com/nhnacademy/taskAPI/exception/ErrorResponse.java
@@ -2,13 +2,13 @@ package com.nhnacademy.taskAPI.exception;
 
 public record ErrorResponse(
         int status,
-        String code,
+        String message,
         String path
 ) {
     public static ErrorResponse of(ErrorCode errorCode, String path) {
         return new ErrorResponse(
                 errorCode.getStatusValue(),
-                errorCode.getCode(),
+                errorCode.getMessage(),
                 path
         );
     }
